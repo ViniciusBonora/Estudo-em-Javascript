588121c279d94554b57a29e0b8d3a5ae14cc5144
Vinicius Rodrigues Bonora <73659064+ViniciusBonora@users.noreply.github.com>
2020-11-05 17:51:34 -0300

    exercicios

--- ex01/script.js
+++ ex01/script.js
@@ -1,7 +1,19 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('foto')
-    var data = new.Date()
+    var data = new Date
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
+    var min = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} : ${min} horas` 
     
+    if(hora >= 0 && hora <12){
+        img.scr = 'fotomanhã.jpg'
+        document.body.style.background = '#f0e30a'
+    } else if (hora >=12 && hora <18) {
+        img.scr = 'fototarde.jpg'
+        document.body.style.background ='#09094e'
+    } else {
+        img.scr= 'fotonoite.jpg'
+        document.body.style.background = '#2d2d30'
+    }
+}
