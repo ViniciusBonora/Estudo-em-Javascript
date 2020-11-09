ed1be61c30a99f1f03948434a3a5b79813c122db
Vinicius Rodrigues Bonora <73659064+ViniciusBonora@users.noreply.github.com>
2020-11-09 17:23:20 -0300

    exercicio 3 completo

--- ex03/script.js
+++ ex03/script.js
@@ -1,10 +1,10 @@
-function contar(){
+function contar() {
     let ini = window.document.querySelector('input#txtini')
     let fim = window.document.querySelector('input#txtfim')
     let pas = window.document.querySelector('input#txtpas')
     let res = document.getElementById('res')
     //res.innerHTML = `${ini} e ${fim} e ${pas}`
-    if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
         res.innerHTML = `[ERROR] Faltam dados`
     } else {
         res.innerHTML = `Contando...`
@@ -12,9 +12,30 @@ function contar(){
         let f = Number(fim.value)
         let p = Number(pas.value)
 
-        for(let c = i ; c <= f ; c += p){
-            res.innerHTML += `${c}`
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c}`
+            }
+            /*
+            let c = i
+            while(c <= f){
+                res.innerHTML += `${c}`
+                c += p
+            }
+            if(i < f){
+            let c = i
+                do{
+                    res.innerHTML += `${c}`
+                    c += p
+                }while(c <= f)
+            */
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c}`
+            }
         }
+        res.innerHTML += ` \u{1F3C1}`
     }
-    
+
+
 } 
