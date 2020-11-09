7b35f088e7e98fe9148512d90a1d8dd1d8a8342c
Vinicius Rodrigues Bonora <73659064+ViniciusBonora@users.noreply.github.com>
2020-11-09 16:27:36 -0300

    exercicio3

--- ex03/script.js
+++ ex03/script.js
@@ -1,14 +1,20 @@
 function contar(){
-    var ini = window.document.querySelector('input#txtini')
-    var ini = Number(ini.value)
-    var fim = window.document.querySelector('input#txtfim')
-    var fim = Number(fim.value)
-    var pas = window.document.querySelector('input#txtpas')
-    var pas = Number(pas.value)
-    var res = document.getElementById('res')
+    let ini = window.document.querySelector('input#txtini')
+    let fim = window.document.querySelector('input#txtfim')
+    let pas = window.document.querySelector('input#txtpas')
+    let res = document.getElementById('res')
     //res.innerHTML = `${ini} e ${fim} e ${pas}`
-    for(ini; ini <= fim; pas++){
-        res.innerHTML = `${pas}`
+    if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+        res.innerHTML = `[ERROR] Faltam dados`
+    } else {
+        res.innerHTML = `Contando...`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+
+        for(let c = i ; c <= f ; c += p){
+            res.innerHTML += `${c}`
+        }
     }
     
 } 
