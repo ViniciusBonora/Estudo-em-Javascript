2beddfd95aac8b37a95fc98c3912626825acf032
Vinicius Rodrigues Bonora <73659064+ViniciusBonora@users.noreply.github.com>
2020-11-11 19:32:41 -0300

    Impar ou Par

---
+++ ex10/script.js
@@ -0,0 +1,13 @@
+function verificar(n){
+    let num = document.querySelector('input#txtn')
+    let res = document.querySelector('div#res')
+    if(num.value.length == 0 ){
+        window.alert('Preencha todos os daods')
+    } else {
+        let n = Number(num.value)
+        //res.innerHTML = `${n}`
+        if(n )
+       
+        
+    }   
+}
