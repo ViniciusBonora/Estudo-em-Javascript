e610b864747825df7eeb2167e3f63f745099c221
Vinicius Rodrigues Bonora <73659064+ViniciusBonora@users.noreply.github.com>
2020-11-14 14:46:52 -0300

    Asteriscos

--- ex14/script.js
+++ ex14/script.js
@@ -8,10 +8,19 @@ function ast(){
     } else {
         let n = Number(num.value)
         let last = ''
-        for(let cont = 1; cont <= n; cont++){
-            last += '*'         
-            res.innerHTML += `<br>${last}`
+        if(n % 2 == 0){
+            for(let cont = 1; cont <= n-1; cont++){
+                last += '*'         
+                res.innerHTML += `<br>${last}`
+            }
         }
+        else{
+            for(let cont = 1; cont <= n-1; cont++){
+                last += '*'         
+                res.innerHTML += `<br>${last}`
+            }
+        }
+
     }
     num.value = ''
 }
