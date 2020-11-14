4c184f1b559b2df34613d57f69fca62ef89b234e
Vinicius Rodrigues Bonora <73659064+ViniciusBonora@users.noreply.github.com>
2020-11-14 13:40:46 -0300

    Asteriscos

--- ex14/script.js
+++ ex14/script.js
@@ -1,14 +1,17 @@
 function ast(){
+    
     let num = document.getElementById('txtn')
     let res = document.getElementById('res')
+    res.innerHTML = '';
     if(num.value.length == 0){
         window.alert('Digite um numero')
     } else {
         let n = Number(num.value)
-        let ast = '*'
+        let last = ''
         for(let cont = 1; cont <= n; cont++){
-            ast += ast
-            res.innerHTML += `<br>${ast}`
+            last += '*'         
+            res.innerHTML += `<br>${last}`
         }
     }
+    num.value = ''
 }
