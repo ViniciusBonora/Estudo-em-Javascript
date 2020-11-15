ce81c491ee82adf5579a6ccda658f5f9c41e2a0c
Vinicius Rodrigues Bonora <73659064+ViniciusBonora@users.noreply.github.com>
2020-11-15 14:21:33 -0300

    Numeros Primos

---
+++ ex15/script.js
@@ -0,0 +1,13 @@
+function verificar(){
+    let num = document.getElementById('txtn')
+    let res = document.getElementById('res')
+    res.innerHTML = '';
+    if(num.value.length == 0){
+        window.alert ('Preencha o campo limite')
+    } else {
+        let lim = Number(num.value)
+        //res.innerHTML= `${lim}`
+    }   for(let i = 0; lim <= i; i++){
+        if(i % 2 == 0)
+    }
+} 
